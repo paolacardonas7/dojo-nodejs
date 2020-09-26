05ef8b3d16a444e939fdbca9ba2814933e0db65c
Paola Andrea Cardona Salcedo <pao_cardona97@hotmail.com>
2020-09-26 09:39:31 -0500

    spotifai service 2

---
+++ app/config/db.config.js
@@ -0,0 +1,5 @@
+
+uri = "mongodb+srv://paocs:7067@cluster0.tztuc.mongodb.net/spoti-fai?retryWrites=true&w=majority"
+
+
+module.exports = uri;
---
+++ app/tests/tracks.test.js
@@ -0,0 +1,11 @@
+
+const assert = require("chai").assert;
+const getTracks = require("../services/spotify/spotify.service.js");
+
+
+describe("Sporifi services", function () {
+    it("get tracks", async function () {
+        const result = await getTracks("bad bunny");
+        console.log(result);
+    });
+}); 
